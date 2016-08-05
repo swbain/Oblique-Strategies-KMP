e696a899ef85f663a635266b1899fdac6bb30752
Stephen Bain <swbain@gmail.com>
2016-08-05 00:40:09 -0500

    implemented butterknife

--- app/src/main/java/com/stephenbain/obliquestrategies/view/HomeActivity.java
+++ app/src/main/java/com/stephenbain/obliquestrategies/view/HomeActivity.java
@@ -3,6 +3,9 @@ package com.stephenbain.obliquestrategies.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import butterknife.BindView;
 import com.stephenbain.obliquestrategies.MyApplication;
 import com.stephenbain.obliquestrategies.R;
 
@@ -11,6 +14,9 @@ import javax.inject.Inject;
 public class HomeActivity extends AppCompatActivity implements HomePresenter.HomeView {
 
     @Inject HomePresenter presenter;
+
+    @BindView(R.id.touchIntercept) View touchIntercept;
+    @BindView(R.id.strategy) TextView strategy;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
