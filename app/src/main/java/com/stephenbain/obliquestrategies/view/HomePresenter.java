a43edb18407de5e8ea4ff370ef6efc36a7153627
Stephen Bain <swbain@gmail.com>
2016-08-05 00:27:53 -0500

    forgot inject annotation...dumb

--- app/src/main/java/com/stephenbain/obliquestrategies/view/HomePresenter.java
+++ app/src/main/java/com/stephenbain/obliquestrategies/view/HomePresenter.java
@@ -2,11 +2,18 @@ package com.stephenbain.obliquestrategies.view;
 
 import com.stephenbain.obliquestrategies.util.NullObject;
 
+import javax.inject.Inject;
+
 public class HomePresenter {
 
     private static final HomeView NULL_VIEW = NullObject.create(HomeView.class);
 
     private HomeView view = NULL_VIEW;
+
+    @Inject
+    public HomePresenter() {
+
+    }
 
     public void attach(HomeView view) {
         this.view = view;
