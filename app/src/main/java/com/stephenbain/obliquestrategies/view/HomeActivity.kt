003a403f37fc5dc1ea9c069891191229136bde1b
Stephen Bain <swbain@gmail.com>
2017-06-16 20:11:55 -0500

    passing text from presenter

--- app/src/main/java/com/stephenbain/obliquestrategies/view/HomeActivity.kt
+++ app/src/main/java/com/stephenbain/obliquestrategies/view/HomeActivity.kt
@@ -3,12 +3,27 @@ package com.stephenbain.obliquestrategies.view
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.stephenbain.obliquestrategies.R
+import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), HomePresenter.HomeView {
+
+    private val presenter = HomePresenter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        presenter.attach(this)
+        presenter.present()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.detach()
+    }
+
+    override fun setStrategy(strategy: String) {
+        this.strategy.text = strategy
     }
 
 }
