b0f734ef0fc2f25b85028d24fa7374f8d64e1d84
Stephen Bain <swbain@gmail.com>
2017-06-17 01:33:23 -0500

    implemented strategy service, finished kotlin conversion

--- app/src/main/java/com/stephenbain/obliquestrategies/di/AppModule.kt
+++ app/src/main/java/com/stephenbain/obliquestrategies/di/AppModule.kt
@@ -6,6 +6,8 @@ import com.stephenbain.obliquestrategies.data.StrategyService
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module class AppModule(val application: Application) {
@@ -13,7 +15,11 @@ import javax.inject.Singleton
     @Provides @Singleton fun provideContext() : Context = application
 
     @Provides @Singleton fun provideStrategyService() : StrategyService {
-        val retrofit = Retrofit.Builder().baseUrl("http://brianeno.needsyourhelp.org/").build()
+        val retrofit = Retrofit.Builder()
+                .baseUrl("http://brianeno.needsyourhelp.org/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
 
         return retrofit.create(StrategyService::class.java)
     }
