cf89e1b30abe4aeff123596fc6a43149e95059dc
Stephen Bain <swbain@gmail.com>
2017-06-17 00:56:54 -0500

    fixed inject for HomeActivity

--- app/src/main/java/com/stephenbain/obliquestrategies/view/HomeActivity.kt
+++ app/src/main/java/com/stephenbain/obliquestrategies/view/HomeActivity.kt
@@ -2,17 +2,19 @@ package com.stephenbain.obliquestrategies.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.stephenbain.obliquestrategies.MyApplication
 import com.stephenbain.obliquestrategies.R
 import kotlinx.android.synthetic.main.activity_home.*
+import javax.inject.Inject
 
 class HomeActivity : AppCompatActivity(), HomePresenter.HomeView {
 
-    private val presenter = HomePresenter()
+    @Inject lateinit var presenter: HomePresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        (application as MyApplication).component.inject(this)
         setContentView(R.layout.activity_home)
-
         presenter.attach(this)
         presenter.present()
     }
