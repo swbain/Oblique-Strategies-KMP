3f8f202a6070c6d79fc6a7d211f6cf96486f96d9
Stephen Bain <swbain@gmail.com>
2017-06-17 00:51:54 -0500

    dagger setup

--- app/src/main/java/com/stephenbain/obliquestrategies/MyApplication.kt
+++ app/src/main/java/com/stephenbain/obliquestrategies/MyApplication.kt
@@ -1,7 +1,21 @@
 package com.stephenbain.obliquestrategies
 
 import android.app.Application
+import com.stephenbain.obliquestrategies.di.AppComponent
+import com.stephenbain.obliquestrategies.di.AppModule
+import com.stephenbain.obliquestrategies.di.DaggerAppComponent
 
 class MyApplication : Application() {
+
+    val component: AppComponent by lazy {
+        DaggerAppComponent.builder()
+                .appModule(AppModule(this))
+                .build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        component.inject(this)
+    }
 
 }
---
+++ app/src/main/java/com/stephenbain/obliquestrategies/di/AppModule.kt
@@ -0,0 +1,13 @@
+package com.stephenbain.obliquestrategies.di
+
+import android.app.Application
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module class AppModule(val application: Application) {
+
+    @Provides @Singleton fun provideContext() : Context = application
+
+}
