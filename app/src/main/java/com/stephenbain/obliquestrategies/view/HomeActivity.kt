2e562f22453d86ca527a7f5928a0baf3f86e2962
Stephen Bain <swbain@gmail.com>
2017-06-16 19:48:36 -0500

    kotlin hello world

---
+++ app/src/main/java/com/stephenbain/obliquestrategies/view/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.stephenbain.obliquestrategies.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.stephenbain.obliquestrategies.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+}
