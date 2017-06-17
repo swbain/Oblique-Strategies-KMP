6d635cae1fa36d810c847aef83853f82dc547a17
Stephen Bain <swbain@gmail.com>
2017-06-16 19:48:36 -0500

    kotlin hello world

--- app/src/main/java/com/stephenbain/obliquestrategies/MyApplication.java
+++ app/src/main/java/com/stephenbain/obliquestrategies/MyApplication.java
@@ -1,27 +1,6 @@
 package com.stephenbain.obliquestrategies;
 
 import android.app.Application;
-import com.stephenbain.obliquestrategies.di.AppComponent;
-import com.stephenbain.obliquestrategies.di.AppModule;
-import com.stephenbain.obliquestrategies.di.DaggerAppComponent;
 
 public class MyApplication extends Application {
-
-    private static final AppComponent COMPONENT;
-    private static final AppModule MODULE;
-
-    static {
-        COMPONENT = DaggerAppComponent.builder().appModule(MODULE = new AppModule()).build();
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        MODULE.setApplication(this);
-    }
-
-    public static AppComponent getComponent() {
-        return COMPONENT;
-    }
 }
