f6a2af1ae787979fb3b0f95c74b34f1b871faea9
Stephen Bain <swbain@gmail.com>
2018-03-20 19:38:15 -0500

    added fields to CardResponse class

--- app/src/main/java/com/stephenbain/obliquestrategies/model/CardResponse.kt
+++ app/src/main/java/com/stephenbain/obliquestrategies/model/CardResponse.kt
@@ -2,5 +2,7 @@ package com.stephenbain.obliquestrategies.model
 
 data class CardResponse(
         val id: String,
+        val edition: String,
+        val cardNumber: Int,
         val strategy: String
 )
